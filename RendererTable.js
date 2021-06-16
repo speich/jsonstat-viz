5628ffc0b698f07c3cf388cef921aab99dfb5790
speich <??Rube//cula.55>
2021-06-16 12:05:02 +0200

    move some methods into own class

--- RendererTable.js
+++ RendererTable.js
@@ -1,289 +1,241 @@
+import {utilArray} from "./utilArray.js";
+
 /**
- * Renders json-stat data as a table.
- * @see www.json-stat.org
- * A table consists of a number of dimensions that are used to define the rows of the table (referred to as label columns)
- * and a number of dimensions that are used to define the columns of the table (referred to as value columns).
- * Setting the property numRowDim defines which dimensions are used for label columns and which for the value columns.
+ * Renders json-stat data as a html table.
  *
- * Note: When rendering a table with rowspans (setting the useRowSpans property to true),
- * applying css might become complicated because of the irregular number of cells per row.
+ * A table consists of a number of dimensions that are used to define the rows of the two-dimensional table
+ * (referred to as row dimensions) and a number of dimensions that are used to define the columns of the table
+ * (referred to as col dimensions). Each row dimension creates its own pre column, containing only category labels,
+ * whereas the column dimensions contain the actual values.
+ *
+ * Setting the property numRowDim (number of row dimensions) defines how many of the dimensions are use for the rows,
+ * beginning at the start of the ordered size array of the jsonstat schema. Remaining dimensions are used for columns.
+ * Dimensions of length one are excluded.
+ *
+ * Setting the property noLabelLastDim will skip the row in the table heading containing the labels of the last
+ * dimension.
+ * @see www.json-stat.org
  */
 export class RendererTable {
-  // TODO: auto: if dimension has role attribute 'geo' use it as the columns of the table
 
-  /**
-   * @property {Array} colDims dimensions used for columns
-   * @property {Array} rowDims column dimensions used for rows
-   * @property {JsonStat} jsonstat
-   * @property {Number} numRowDim number of row dimensions
-   * @property {HTMLTableElement} table
-   * @property {Boolean} useRowSpans use rowspan for empty label cells?
-   * @param {JsonStat} jsonstat
-   * @param {Number} numRowDim number of row dimensions
-   */
-  constructor(jsonstat, numRowDim) {
-    let dims = jsonstat.getDimensionSizes();
-
-    this.jsonstat = jsonstat;
-    this.numRowDim = numRowDim;
-    this.rowDims = this.getDims(dims, 'row');
-    this.colDims = this.getDims(dims, 'col');
-    this.table = document.createElement('table');
-    this.table.classList.add('jst-viz');
-    this.table.classList.add('rowdims' + this.rowDims.length);
-    this.table.classList.add('lastdim' + dims[dims.length - 1]);
-    this.noLabelLastDim = true;
-    this.useRowSpans = false;
-  }
-
-  init() {
-    // cache some often used numbers before rendering table
-    let dimsAll = this.jsonstat.getDimensionSizes(false);
-
-    this.numOneDim = dimsAll.length - this.rowDims.length - this.colDims.length;
-    this.numValueCols = this.colDims.length > 0 ? RendererTable.product(this.colDims) : 1;
-    this.numLabelCols = this.rowDims.length;
-    this.numHeaderRows = this.colDims.length > 0 ? this.colDims.length * 2 : 1; // add an additional row to label each dimension
-  }
-
-  /**
-   * Returns the dimensions that can be used for rows or cols.
-   * Constant dimensions (e.g. of length 1) are excluded.
-   * @param {Array} dims
-   * @param {String} type 'row' or 'col'
-   */
-  getDims(dims, type) {
-
-    return type === 'row' ? dims.slice(0, this.numRowDim) : dims.slice(this.numRowDim);
-  }
-
-  /**
-   * Calculate the product of all array elements.
-   * @param {Array} values
-   */
-  static product(values) {
-    if (values.length > 0) {
-
-      return values.reduce((a, b) => a * b);
-    } else {
-
-      return values;
-    }
-  }
-
-  /**
-   * Calculate two products from array values.
-   * The first returned value is the product of all values with an element index equal or higher than the passed one, the
-   * second is the product of all values with an index higher. If it is the last element then the product is 1.
-   * @param {Array} values
-   * @param idx element index
-   * @return {Array}
-   * @private
-   */
-  _partials(values, idx) {
-    let f = [];
-
-    f[0] = RendererTable.productUpper(values, idx);
-    f[1] = idx < values.length ? RendererTable.productUpper(values, idx + 1) : 1;
-
-    return f;
-  }
-
-  /**
-   * Calculates the product of all array values with an element index equal or higher than the passed one.
-   * @param {Array} values
-   * @param idx element index
-   * @return {number}
-   */
-  static productUpper(values, idx) {
-    let num = 1,
-      len = values.length;
-
-    for (let i = idx; i < len; i++) {
-      num *= values[i];
+    /**
+     * @property {Array} colDims dimensions used for columns containing values
+     * @property {Array} rowDims dimensions used for rows containing labels, that make up the rows
+     * @property {JsonStatReader} jsonStatReader jsonStatReader schema
+     * @property {Number} numRowDim number of row dimensions
+     * @property {HTMLTableElement} table
+     * @property {Boolean} nolabelLastDim render row with labels of last dimension?
+     * @param {JsonStatReader} jsonStatReader jsonStatReader schema
+     * @param {Number} numRowDim number of row dimensions
+     */
+    constructor(jsonStatReader, numRowDim) {
+        this.colDims = [];
+        this.rowDims = [];
+        this.jsonstat = jsonStatReader;
+        this.numRowDim = numRowDim;
+        this.table = document.createElement('table');
+        this.table.classList.add('jst-viz');
+        this.noLabelLastDim = true;
     }
 
-    return num;
-  }
+    /**
+     * Initialize properties before rendering the table.
+     */
+    init() {
+        // cache some often used numbers before rendering table
+        let dimsAll = this.jsonstat.getDimensionSizes(false),
+            dims = this.jsonstat.getDimensionSizes();
 
-  /**
-   * Renders the data as a html table.
-   * Reads the value array and renders it as a table.
-   * @return {HTMLTableElement}
-   */
-  render() {
-    this.init();
-    this.caption();
-    this.rowHeaders();
-    this.rows();
-
-    return this.table;
-  }
-
-  /**
-   * Creates the table head and appends header cells, row by row to it.
-   */
-  rowHeaders() {
-    let row, tHead;
-
-    tHead = this.table.createTHead();
-    for (let rowIdx = 0; rowIdx < this.numHeaderRows; rowIdx++) {
-      if (this.noLabelLastDim === false || rowIdx !== this.numHeaderRows - 2) {
-        row = tHead.insertRow();
-        this.headerLabelCells(row, rowIdx);
-        this.headerValueCells(row, rowIdx);
-      }
-    }
-  }
-
-  /**
-   * Creates the table body and appends table cells row by row to it.
-   */
-  rows() {
-    let tBody, row;
-
-    tBody = this.table.createTBody();
-    for (let offset = 0, len = this.jsonstat.getNumValues(); offset < len; offset++) {
-      if (offset % this.numValueCols === 0) {
-        row = tBody.insertRow();
-        this.labelCells(row);
-      }
-      this.valueCells(row, offset);
-    }
-  }
-
-  /**
-   * Creates the cells for the headers of the label columns
-   * @param {HTMLTableRowElement} row
-   * @param rowIdx
-   */
-  headerLabelCells(row, rowIdx) {
-    for (let k = 0; k < this.numLabelCols; k++) {
-      let cell, label = null, scope = null;
-
-      if (rowIdx === this.numHeaderRows - 1) { // last header row
-        label = this.jsonstat.getLabel(this.numOneDim + k);
-        scope = 'col';
-      }
-      cell = RendererTable.headerCell(label, scope);
-      row.appendChild(cell);
-    }
-  }
-
-  /**
-   * Creates the cells for the headers of the value columns.
-   * @param {HTMLTableRowElement} row
-   * @param rowIdx
-   */
-  headerValueCells(row, rowIdx) {
-    let cell, z, idx, dimIdx, f, catIdx, label, colspan, scope;
-
-    if (this.colDims.length === 0) {
-      cell = RendererTable.headerCell();
-      row.appendChild(cell);
-
-      return;
+        this.rowDims = this.getDims(dims, 'row');
+        this.colDims = this.getDims(dims, 'col');
+        this.numOneDim = dimsAll.length - dims.length;
+        this.numValueCols = this.colDims.length > 0 ? utilArray.product(this.colDims) : 1;
+        this.numLabelCols = this.rowDims.length;
+        this.numHeaderRows = this.colDims.length > 0 ? this.colDims.length * 2 : 1; // add an additional row to label each dimension
     }
 
-    idx = Math.floor(rowIdx / 2); // 0,1,2,3,... -> 0,0,1,1,2,2,...
-    dimIdx = this.numOneDim + this.numRowDim + idx;
-    f = this._partials(this.colDims, idx);
-    for (let i = 0; i < this.numValueCols; i++) {
-      colspan = null;
-      scope = 'col';
-      z = rowIdx % 2;
-      if (z === 0) {
-        label = this.jsonstat.getLabel(dimIdx);
-      } else {
-        catIdx = Math.floor((i % f[0]) / f[1]);
-        label = this.jsonstat.getCategoryLabel(dimIdx, catIdx);
-      }
-      if (f[z] > 1) {
-        colspan = f[z];
-        i += colspan - 1; // colspan - 1 -> i++ follows
-        scope = 'colgroup';
-      }
-      cell = RendererTable.headerCell(label, scope, colspan);
-      row.appendChild(cell);
-    }
-  }
+    /**
+     * Returns the dimensions that can be used for rows or cols.
+     * Constant dimensions (e.g. of length 1) are excluded.
+     * @param {Array} dims
+     * @param {String} type 'row' or 'col'
+     */
+    getDims(dims, type) {
 
-  /**
-   * Appends the label of the row.
-   * Inserts the label as a HTMLTableHeaderEleem
-   * @param {HTMLTableRowElement} row
-   */
-  labelCells(row) {
-    let cell, rowIdx, f, catIdx, label, rowspan, numVirtRow, scope;
-
-    numVirtRow = this.noLabelLastDim ? 1 : 0;
-    rowIdx = row.rowIndex - this.numHeaderRows + numVirtRow;
-    for (let i = 0; i < this.numLabelCols; i++) {
-      f = this._partials(this.rowDims, i);
-      catIdx = Math.floor(rowIdx % f[0] / f[1]);
-      label = rowIdx % f[1] === 0 ? this.jsonstat.getCategoryLabel(this.numOneDim + i, catIdx) : null;
-      rowspan = null;
-      scope = 'row';
-      if (this.useRowSpans && f[1] > 1) {
-        rowspan = f[1];
-        scope = 'rowgroup';
-      }
-      if (rowIdx % f[1] === 0 || !this.useRowSpans) {
-        cell = RendererTable.headerCell(label, scope, null, rowspan);
-        row.appendChild(cell);
-      }
-    }
-  }
-
-  /**
-   * Appends the value at offset as a cell.
-   * Inserts a HTMLTableCellElement at the end of the row with a value taken from the values at given offset.
-   * @param {HTMLTableRowElement} row
-   * @param offset
-   */
-  valueCells(row, offset) {
-    let cell, stat = this.jsonstat;
-
-    cell = row.insertCell();
-    cell.textContent = stat.data.value[offset]; // not need to escape
-  }
-
-  /**
-   * Create and returns a header cell element.
-   * @param {String} [str] cell content
-   * @param {String} [scope] scope of cell
-   * @param [colspan] number of columns to span
-   * @param [rowspan] number of rows to span
-   * @return {HTMLTableCellElement}
-   */
-  static headerCell(str = null, scope = null, colspan = null, rowspan = null) {
-    let cell = document.createElement('th');
-
-    if (scope !== null) {
-      cell.scope = scope;
-    }
-    if (str !== null) {
-      cell.innerHTML = str;
-    }
-    if (colspan !== null) {
-      cell.colSpan = colspan;
-    }
-    if (rowspan !== null) {
-      cell.rowSpan = rowspan;
+        return type === 'row' ? dims.slice(0, this.numRowDim) : dims.slice(this.numRowDim);
     }
 
-    return cell;
-  }
+    /**
+     * Renders the data as a html table.
+     * Reads the value array and renders it as a table.
+     * @return {HTMLTableElement}
+     */
+    render() {
+        this.init();
+        this.caption();
+        this.rowHeaders();
+        this.rows();
 
-  /**
-   * Creates and inserts a caption.
-   * @return {HTMLTableCaptionElement}
-   */
-  caption() {
-    let caption = this.table.createCaption();
+        return this.table;
+    }
 
-    caption.innerHTML = this.jsonstat.escapeHtml(this.jsonstat.data.label);
+    /**
+     * Creates the table head and appends header cells, row by row to it.
+     */
+    rowHeaders() {
+        let row, tHead;
 
-    return caption;
-  }
+        tHead = this.table.createTHead();
+        for (let rowIdx = 0; rowIdx < this.numHeaderRows; rowIdx++) {
+            if (this.noLabelLastDim === false || rowIdx !== this.numHeaderRows - 2) {
+                row = tHead.insertRow();
+                this.headerLabelCells(row, rowIdx);
+                this.headerValueCells(row, rowIdx);
+            }
+        }
+    }
+
+    /**
+     * Creates the table body and appends table cells row by row to it.
+     */
+    rows() {
+        let tBody, row;
+
+        tBody = this.table.createTBody();
+        for (let offset = 0, len = this.jsonstat.getNumValues(); offset < len; offset++) {
+            if (offset % this.numValueCols === 0) {
+                row = tBody.insertRow();
+                this.labelCells(row);
+            }
+            this.valueCell(row, offset);
+        }
+    }
+
+    /**
+     * Creates the cells for the headers of the label columns
+     * @param {HTMLTableRowElement} row
+     * @param rowIdx
+     */
+    headerLabelCells(row, rowIdx) {
+        for (let k = 0; k < this.numLabelCols; k++) {
+            let cell, label = null, scope = null;
+
+            if (rowIdx === this.numHeaderRows - 1) { // last header row
+                label = this.jsonstat.getLabel(this.numOneDim + k);
+                scope = 'col';
+            }
+            cell = RendererTable.headerCell(label, scope);
+            row.appendChild(cell);
+        }
+    }
+
+    /**
+     * Creates the cells for the headers of the value columns.
+     * @param {HTMLTableRowElement} row
+     * @param rowIdx
+     */
+    headerValueCells(row, rowIdx) {
+        let cell, z, idx, dimIdx, f, catIdx, label, colspan;
+
+        if (this.colDims.length === 0) {
+            cell = RendererTable.headerCell();
+            row.appendChild(cell);
+
+            return;
+        }
+
+        idx = Math.floor(rowIdx / 2); // 0,1,2,3,... -> 0,0,1,1,2,2,...
+        dimIdx = this.numOneDim + this.numRowDim + idx;
+        f = utilArray.partials(this.colDims, idx);
+        for (let i = 0; i < this.numValueCols; i++) {
+            colspan = null;
+            z = rowIdx % 2;
+            if (z === 0) {
+                label = this.jsonstat.getLabel(dimIdx);
+            } else {
+                catIdx = Math.floor((i % f[0]) / f[1]);
+                label = this.jsonstat.getCategoryLabel(dimIdx, catIdx);
+            }
+            if (f[z] > 1) {
+                colspan = f[z];
+                i += colspan - 1; // colspan - 1 -> i++ follows
+            }
+            cell = RendererTable.headerCell(label, 'col', colspan);
+            row.appendChild(cell);
+        }
+    }
+
+    /**
+     * Appends the label of the row.
+     * Inserts the label as a HTMLTableHeaderElement
+     * @param {HTMLTableRowElement} row
+     */
+    labelCells(row) {
+        let cell, rowIdx, f, catIdx, label, rowspan, numVirtRow;
+
+        numVirtRow = this.noLabelLastDim ? 1 : 0;
+        rowIdx = row.rowIndex - this.numHeaderRows + numVirtRow;
+        for (let i = 0; i < this.numLabelCols; i++) {
+            f = utilArray.partials(this.rowDims, i);
+            if (rowIdx % f[1] === 0) {
+                catIdx = Math.floor(rowIdx % f[0] / f[1]);
+                label = this.jsonstat.getCategoryLabel(this.numOneDim + i, catIdx);
+                rowspan = f[1] > 1 ? f[1] : null;
+                cell = RendererTable.headerCell(label, 'row', null, rowspan);
+                row.appendChild(cell);
+            }
+        }
+    }
+
+    /**
+     * Appends the value at offset as a cell.
+     * Inserts a HTMLTableCellElement at the end of the row with a value taken from the values at given offset.
+     * @param {HTMLTableRowElement} row
+     * @param offset
+     */
+    valueCell(row, offset) {
+        let cell, stat = this.jsonstat;
+
+        cell = row.insertCell();
+        cell.textContent = stat.data.value[offset]; // no need to escape
+    }
+
+    /**
+     * Create and returns a header cell element.
+     * @param {String} [str] cell content
+     * @param {String} [scope] scope of cell
+     * @param [colspan] number of columns to span
+     * @param [rowspan] number of rows to span
+     * @return {HTMLTableCellElement}
+     */
+    static headerCell(str = null, scope = null, colspan = null, rowspan = null) {
+        let cell = document.createElement('th');
+
+        if (scope !== null) {
+            cell.scope = scope;
+        }
+        if (str !== null) {
+            cell.innerHTML = str;
+        }
+        if (colspan !== null) {
+            cell.colSpan = colspan;
+        }
+        if (rowspan !== null) {
+            cell.rowSpan = rowspan;
+        }
+
+        return cell;
+    }
+
+    /**
+     * Creates and inserts a caption.
+     * @return {HTMLTableCaptionElement}
+     */
+    caption() {
+        let caption = this.table.createCaption();
+
+        caption.innerHTML = this.jsonstat.escapeHtml(this.jsonstat.data.label);
+
+        return caption;
+    }
 }
